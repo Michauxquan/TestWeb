093f8904679a69cdfe5fee351a054445607e5589
yytt0419 <2801595467@qq.com>
2017-02-09 17:52:23 +0800

    f 新增竞猜业务数据处理

---
+++ OWZX/Libraries/OWZX.Core/Data/RDBS/IRDBSStrategy/ILotteryListStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWZX.Core
+{
+    /// <summary>
+    /// 新的竞猜数据接口
+    /// </summary>
+    public partial interface IRDBSStrategy
+    {
+        #region 竞猜数据
+        /// <summary>
+        /// 根据类型获取竞猜首页数据
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        DataSet GetLotteryByType(string type, string pageindex, string pagesize);
+
+        #endregion
+    }
+}
---
+++ OWZX/Strategies/OWZX.RDBSStrategy.SqlServer/LotteryListStrategy.cs
@@ -0,0 +1,29 @@
+﻿using OWZX.Core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWZX.RDBSStrategy.SqlServer
+{
+    /// <summary>
+    /// SqlServer策略之新的竞猜
+    /// </summary>
+    public partial class RDBSStrategy : IRDBSStrategy
+    {
+        #region 竞猜数据
+        /// <summary>
+        /// 根据类型获取竞猜首页数据
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public DataSet GetLotteryByType(string type, string pageindex, string pagesize)
+        {
+            string sql = string.Format(@"");
+            return RDBSHelper.ExecuteDataset(sql);
+        }
+        #endregion
+    }
+}
