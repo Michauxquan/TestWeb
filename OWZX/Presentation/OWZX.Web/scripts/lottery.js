2b0ac966ffedd53efec0099ab444ca9a87828837
yytt160419 <2801595467@qq.com>
2017-02-19 11:23:17 +0800

    [b] lot

--- OWZX/Presentation/OWZX.Web/scripts/lottery.js
+++ OWZX/Presentation/OWZX.Web/scripts/lottery.js
@@ -1,6 +1,6 @@
 ﻿/*竞猜*/
 /*倒计时 剩余总时间，期号，下注时间，封盘时间*/
-function GetRTime(type, ctime, fcnum, bettime, stoptime)
+function GetRTime(type, ctime, fcnum, totalime, stoptime)
 {
     num = fcnum;
     //ns 开奖时间
@@ -8,13 +8,14 @@ function GetRTime(type, ctime, fcnum, bettime, stoptime)
     if (nS > 0)
     {
         nS = nS - 1;
-        if (nS >= stoptime && nS <= bettime)
+        if (nS > stoptime && nS <= totalime)
         {
             //bett
             $(".remains").html(
                 '第 <i class="bold">' + fcnum + '</i>期  还有<span class="ltwarn">' + (nS - 30).toString() + '</span>秒停止下注!');
             StrTimeOut = 1;
-        } else
+        }
+        else if (nS > 0 && nS <= stoptime)
         {
             //stop
             if (nS != 0)
@@ -26,7 +27,7 @@ function GetRTime(type, ctime, fcnum, bettime, stoptime)
                 $(".remains").html(
                                 '第 <i class="bold">' + fcnum + '</i>期  正在开奖,请稍后!');
             }
-            if ($(".lot_btn_" + fcnum + " .ltoperate").text() != "正在开奖")
+            if ($(".lot_btn_" + fcnum + " .ltoperate").text().toString().trim() != "正在开奖")
             {
                 $(".lot_btn_" + fcnum).html("").html('<a href="#"><div class="ltoperate">正在开奖</div></a>');
             }
@@ -47,6 +48,7 @@ function GetRTime(type, ctime, fcnum, bettime, stoptime)
 
         if (nS == -5 || nS == -10 || nS == -15 || nS == -20 || nS == -25 || nS == -30)
         {
+            
             $(".remains").html('Loading......');
             $.post("/nwlottery/lotteryopen", { "type": lotterytype, "expect": num }, function (data)
             {
@@ -54,6 +56,8 @@ function GetRTime(type, ctime, fcnum, bettime, stoptime)
                 //未开奖 显示新一期的倒计时，另起一个计时器，获取未开奖信息直到获取到结果
                 if (data == "1")
                 {
+                    clearTimeout(tiner);
+
                     if ($(".sec_head a:eq(0)").hasClass("hot"))
                     {
                         $(".lot_content").load("/nwlottery/_index", { "type": lotterytype });
@@ -62,6 +66,7 @@ function GetRTime(type, ctime, fcnum, bettime, stoptime)
                         $(".remains").html('第 <i class="bold">' + fcnum + '</i>期  已开奖,请刷新!');
                         //$(".temp_content").load("/nwlottery/_content", { "type": lotterytype, "page": 1 });
                     }
+                    return;
                 } else
                 {
                     if (nS == -30)
@@ -84,7 +89,7 @@ function GetRTime(type, ctime, fcnum, bettime, stoptime)
             clearTimeout(tiner);
     }
     if (nS > -30)
-        tiner = setTimeout("GetRTime(" + type + "," + nS + "," + fcnum + "," + bettime + "," + stoptime + ")", 1000);
+        tiner = setTimeout("GetRTime(" + type + "," + nS + "," + fcnum + "," + totalime + "," + stoptime + ")", 1000);
 }
 var traptime;
 function trap(type,fcnum)
